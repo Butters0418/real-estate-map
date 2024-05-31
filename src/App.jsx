ffba3c00680c356694b4ebb700c1d45eba8f318a
Butters <swki418@gmail.com>
2024-05-31 11:07:05 +0800

    Change 設定vite.config 及 package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { sky } from './assets/layout/index';
 function App() {
-  return <div className="wrap">APP</div>;
+  return (
+    <div className="wrap">
+      <div className="mx-auto max-w-[500px]">
+        <img src={sky} alt="sky" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/layout/index.js
@@ -0,0 +1,4 @@
+import sky from './sky.jpg';
+import skywebp from './skyWebp.webp';
+
+export { sky, skywebp };
